f616fa1d67b0474a5eadef835d1ccc600ae23bd5
Kevin Quiroz <kquirozg@eafit.edu.co>
2024-04-09 14:10:25 -0500

    Play bottom added

---
+++ Assets/Crowd Runner/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public enum GameState { Menu, Game, LevelComplete, Gameover }
+
+    private GameState gameState;
+
+    public static Action<GameState> onGameStateChanged;
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+            instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetGameState(GameState gameState)
+    {
+        this.gameState = gameState;
+        onGameStateChanged?.Invoke(gameState);
+
+        Debug.Log("Game State changed to : " + gameState);
+    }
+}
